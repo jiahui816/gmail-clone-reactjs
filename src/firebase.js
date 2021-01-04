34095e003410f4318ddf033c6e8fb44b23e91ce1
Jia Hui Liou <30471836+jiahui816@users.noreply.github.com>
2021-01-04 16:47:32 +1100

    initialise project

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB_IMt3Iy0jOMeUEJCXZxrMW9da6aD1q2I",
+  authDomain: "clone-78f20.firebaseapp.com",
+  projectId: "clone-78f20",
+  storageBucket: "clone-78f20.appspot.com",
+  messagingSenderId: "740930438732",
+  appId: "1:740930438732:web:0614a8b7af738b1b7d56e9",
+  measurementId: "G-96NM3MTNYZ",
+};
+
+db = firebaseConfig();
+export { db, auth };
