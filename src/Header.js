7d73abff655ae42b2cb8733c784a6cddb003b103
Jia Hui Liou <30471836+jiahui816@users.noreply.github.com>
2021-01-04 23:41:17 +1100

    Sidebar Header Mail Section

---
+++ src/Header.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Header.css";
+import MenuIcon from "@material-ui/icons/Menu";
+import { Avatar, IconButton } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import AppsIcon from "@material-ui/icons/Apps";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <img
+          src="https://i.pinimg.com/originals/ae/47/fa/ae47fa9a8fd263aa364018517020552d.png"
+          alt="Gmail"
+        />
+      </div>
+      <div className="header__middle">
+        <SearchIcon />
+        <input placeholder="Search mail" type="text" />
+        <ArrowDropDownIcon className="header__inputCaret" />
+      </div>
+      <div className="header__right">
+        <AppsIcon />
+        <NotificationsIcon />
+        <Avatar />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/Mail.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Mail.css";
+function Mail() {
+  return (
+    <div className="mail">
+      <h2>Hello</h2>
+    </div>
+  );
+}
+
+export default Mail;
